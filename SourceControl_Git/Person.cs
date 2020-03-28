55022ed0bd9ea894ada54353dda1057b669ef5fd
Natasha Karachanakova <Natasha.Karachanakova@endava.com>
2020-03-28 18:45:37 +0100

    class Person added

---
+++ SourceControl_Git/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControl_Git
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string PrintPerson()
+        {
+            return this.Name + this.Age;
+        }
+    }
+
+}
--- SourceControl_Git/Program.cs
+++ SourceControl_Git/Program.cs
@@ -10,6 +10,11 @@ namespace SourceControl_Git
     {
         static void Main(string[] args)
         {
+
+            Person p = new Person("Natasha", 21);
+            Person a = new Person("Sara", 19);
+            p.PrintPerson();
+            a.PrintPerson();
         }
     }
 }
