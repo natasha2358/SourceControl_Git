7e28455fdd4d1509e07d6091994288a9c4beb0bf
Natasha Karachanakova <Natasha.Karachanakova@endava.com>
2020-03-28 18:48:26 +0100

    print method chanched

--- SourceControl_Git/Person.cs
+++ SourceControl_Git/Person.cs
@@ -19,7 +19,7 @@ namespace SourceControl_Git
 
         public void PrintPerson()
         {
-             Console.WriteLine("Hello from "+this.Name;
+             Console.WriteLine("Hello from "+ this.Name + "I am "+ this.Age + "years old!");
         }
     }
 
